0842b713ec6b6cbb5f6b1a7db343b45c90babdf0
B-i-x <bix.romero@gmail.com>
2023-01-08 17:49:53 -0700

    yessirr

--- Post Processing Code/linear encoder post processing/test processor.py
+++ Post Processing Code/linear encoder post processing/test processor.py
@@ -8,7 +8,15 @@ with open(DATA_FILE_PATH) as f:
     #print(data_reading_rate_hz)
     second = next(f)
     pin4, pin5, pin6, pin7 = [x for x in second.split(",")]
+
     arr = np.genfromtxt(f, delimiter=",") # no skiprows here
 
-print(arr.shape[0])
-timestamped = np.array(np.arange(arr.shape[0]), arr)
+num_of_rows = np.arange(arr.shape[0]).reshape(arr.shape[0], 1)
+
+temp_time_array = num_of_rows / np.full((arr.shape[0], 1), data_reading_rate_hz)
+
+data_with_time = np.hstack((temp_time_array, arr))
+
+print(temp_time_array)
+
+print(data_with_time)

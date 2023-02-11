4d95ca371606a35a33ea3fc25016be96a1bedd7e
dshay22 <124942574+dshay22@users.noreply.github.com>
2023-02-11 15:51:48 -0700

    Py qt interface (#44)
    * askdnasd
    
    * asdj
    
    * I fixed Alex's stupid lists

--- source/pyqt/current_main.py
+++ source/pyqt/current_main.py
@@ -11,8 +11,15 @@ class Widget(QtWidgets.QWidget):
         super(Widget, self).__init__(parent)
 
         self.graph = pg.PlotWidget()
+        pen = pg.mkPen(color=(255, 0, 0))
+        pen2 = pg.mkPen(color=(0, 255, 0))
+        pen3 = pg.mkPen(color=(0, 0, 255))
+        pen4 = pg.mkPen(color=(255, 0, 255))
         self.serial_handler = SerialHandler(num_of_datapoints = 100)
-        self.data_line =  self.graph.plot(self.serial_handler.x, self.serial_handler.y)
+        self.data_line =  self.graph.plot(self.serial_handler.x, self.serial_handler.y, pen = pen)
+        self.data_line_2 = self.graph.plot(self.serial_handler.x, self.serial_handler.y1, pen = pen2)
+        self.data_line_3 = self.graph.plot(self.serial_handler.x, self.serial_handler.y2, pen = pen3)
+        self.data_line_4 = self.graph.plot(self.serial_handler.x, self.serial_handler.y3, pen = pen4)
         #self.serial_handler.testing()
 
         self.message_le = QtWidgets.QLineEdit()
@@ -37,8 +44,8 @@ class Widget(QtWidgets.QWidget):
         lay.addWidget(self.button)
 
         
-        self.serial = QtSerialPort.QSerialPort(
-            'COM14',
+        self.serial = QtSerialPort.QSerialPort( #connect the arduino
+            'COM6',
             baudRate=QtSerialPort.QSerialPort.BaudRate.Baud115200,
             readyRead=self.receive
         )
@@ -49,11 +56,19 @@ class Widget(QtWidgets.QWidget):
             text = self.serial.readLine().data().decode()
             text = text.rstrip('\r\n')
 
-            self.serial_handler.input_data(text)
+            four_new_y_values = self.serial_handler.input_data(text)
+            
+            self.serial_handler.y = self.serial_handler.ShiftLeft(self.serial_handler.y, four_new_y_values[0])
+            self.serial_handler.y1 = self.serial_handler.ShiftLeft(self.serial_handler.y1, four_new_y_values[1])
+            self.serial_handler.y2 = self.serial_handler.ShiftLeft(self.serial_handler.y2, four_new_y_values[2])
+            self.serial_handler.y3 = self.serial_handler.ShiftLeft(self.serial_handler.y3, four_new_y_values[3])
 
             self.output_te.append(text)
 
             self.data_line.setData(self.serial_handler.x,self.serial_handler.y)
+            self.data_line_2.setData(self.serial_handler.x,self.serial_handler.y1)
+            self.data_line_3.setData(self.serial_handler.x,self.serial_handler.y2)
+            self.data_line_4.setData(self.serial_handler.x,self.serial_handler.y3)
 
     @QtCore.pyqtSlot()
     def send(self):

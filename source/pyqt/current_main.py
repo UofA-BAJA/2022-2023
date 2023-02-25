cb981fbe7b92b2c9830b5664d63b5c26234ecea4
Alex Romero-Lozano <bix.romero@gmail.com>
2023-02-24 20:23:13 -0700

    updating gps (#52)
    * askdnasd
    
    * Py qt interface (#46)
    
    * askdnasd
    
    * asdj
    
    * I fixed Alex's stupid lists
    
    * i delete extra new lines
    
    * Dorian branch (#47)
    
    * askdnasd
    
    * i delete extra new lines
    
    * Hz monitor (#48)
    
    * askdnasd
    
    * i delete extra new lines
    
    * Added a hertz graph and class
    
    Alex wants my mom bad.
    
    * lordy
    
    * Add files via upload
    
    Signed-off-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    
    * Delete lora_transmit.py
    
    Signed-off-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    
    * Add files via upload
    
    Signed-off-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    
    * possibly working (#50)
    
    * Create readme.md (#51)
    
    Signed-off-by: LXL1103 <114650659+LXL1103@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    Signed-off-by: LXL1103 <114650659+LXL1103@users.noreply.github.com>
    Co-authored-by: dshay22 <dorianshay0806@gmail.com>
    Co-authored-by: dshay22 <124942574+dshay22@users.noreply.github.com>
    Co-authored-by: michaelbyerly <91798814+michaelbyerly@users.noreply.github.com>
    Co-authored-by: LXL1103 <114650659+LXL1103@users.noreply.github.com>

--- source/pyqt/current_main.py
+++ source/pyqt/current_main.py
@@ -1,16 +1,20 @@
 from PyQt5 import QtCore, QtWidgets, QtSerialPort
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
-
+from datetime import datetime as dt
+from timeit import default_timer
+import time
 from serial_handler import SerialHandler
+from hertz_rate import Hertz
 
-
+oldtime = 0
 class Widget(QtWidgets.QWidget):
     def __init__(self, parent=None):
-        #alex is stupid haha
+        #alex is stupid
         super(Widget, self).__init__(parent)
 
         self.graph = pg.PlotWidget()
+        self.graph.setTitle("Josh Loves penis")
         pen = pg.mkPen(color=(255, 0, 0))
         pen2 = pg.mkPen(color=(0, 255, 0))
         pen3 = pg.mkPen(color=(0, 0, 255))
@@ -22,8 +26,16 @@ class Widget(QtWidgets.QWidget):
         self.data_line_4 = self.graph.plot(self.serial_handler.x, self.serial_handler.y3, pen = pen4)
         #self.serial_handler.testing()
 
-        self.message_le = QtWidgets.QLineEdit()
+        self.grpah_hz_Monitor = pg.PlotWidget()
+        self.grpah_hz_Monitor.setTitle("Hertz Stream Monitor")
+        self.hertz_plot = Hertz(num_of_datapoints = 100)
+        self.hertz_data = self.grpah_hz_Monitor.plot(self.hertz_plot.x, self.hertz_plot.y, pen = pen)
 
+        self.ot = 0
+
+
+        self.message_le = QtWidgets.QLineEdit()
+   
         self.send_btn = QtWidgets.QPushButton(
             text="Send",
             clicked=self.send
@@ -36,39 +48,61 @@ class Widget(QtWidgets.QWidget):
         )
         lay = QtWidgets.QVBoxLayout(self)
         hlay = QtWidgets.QHBoxLayout()
-        hlay.addWidget(self.message_le)
-        hlay.addWidget(self.send_btn)
+        #hlay.addWidget(self.message_le)
+        #hlay.addWidget(self.send_btn)
         hlay.addWidget(self.graph)
+        hlay.addWidget(self.grpah_hz_Monitor)
+
         lay.addLayout(hlay)
         lay.addWidget(self.output_te)
         lay.addWidget(self.button)
 
+
         
         self.serial = QtSerialPort.QSerialPort( #connect the arduino
-            'COM6',
+            'COM3',
             baudRate=QtSerialPort.QSerialPort.BaudRate.Baud115200,
             readyRead=self.receive
         )
 
     @QtCore.pyqtSlot()
     def receive(self):
-        while self.serial.canReadLine():
-            text = self.serial.readLine().data().decode()
-            text = text.rstrip('\r\n')
-
-            four_new_y_values = self.serial_handler.input_data(text)
+        global oldtime
+        #while self.serial.canReadLine():
+        now = dt.now()
+        current_time_min = int(now.strftime("%M"))
+        current_time_sec = int(now.strftime("%S")) + (current_time_min * 60)
+        current_time_mil = int(now.strftime("%f")) + (current_time_sec * 10**6)
+        seconds = current_time_mil * (10**-6)
             
-            self.serial_handler.y = self.serial_handler.ShiftLeft(self.serial_handler.y, four_new_y_values[0])
-            self.serial_handler.y1 = self.serial_handler.ShiftLeft(self.serial_handler.y1, four_new_y_values[1])
-            self.serial_handler.y2 = self.serial_handler.ShiftLeft(self.serial_handler.y2, four_new_y_values[2])
-            self.serial_handler.y3 = self.serial_handler.ShiftLeft(self.serial_handler.y3, four_new_y_values[3])
+        text = self.serial.readLine().data().decode()
+        text = text.rstrip('\r\n')
 
-            self.output_te.append(text)
+        process(text)
 
-            self.data_line.setData(self.serial_handler.x,self.serial_handler.y)
-            self.data_line_2.setData(self.serial_handler.x,self.serial_handler.y1)
-            self.data_line_3.setData(self.serial_handler.x,self.serial_handler.y2)
-            self.data_line_4.setData(self.serial_handler.x,self.serial_handler.y3)
+        self.output_te.append(f"newline read: {text}")
+        
+        # self.hertz_plot.two_times = self.hertz_plot.ShiftLeft_for_Hertz(self.hertz_plot.two_times, start)
+        dif = seconds - self.ot
+        self.ot = seconds
+        print(f"new = {seconds}, old = {self.ot}, diff = {dif}") #self.hertz_plot.difference(self.hertz_plot.two_times)
+        hrtz = self.hertz_plot.inverse(dif)
+
+        self.hertz_plot.y = self.hertz_plot.ShiftLeft_for_Hertz(self.hertz_plot.y, hrtz)
+        self.hertz_data.setData(self.hertz_plot.x, self.hertz_plot.y)
+        
+        four_new_y_values = self.serial_handler.input_data(text)
+        self.serial_handler.y = self.serial_handler.ShiftLeft(self.serial_handler.y, four_new_y_values[0])
+        self.serial_handler.y1 = self.serial_handler.ShiftLeft(self.serial_handler.y1, four_new_y_values[1])
+        self.serial_handler.y2 = self.serial_handler.ShiftLeft(self.serial_handler.y2, four_new_y_values[2])
+        self.serial_handler.y3 = self.serial_handler.ShiftLeft(self.serial_handler.y3, four_new_y_values[3])
+
+        self.data_line.setData(self.serial_handler.x,self.serial_handler.y)
+        self.data_line_2.setData(self.serial_handler.x,self.serial_handler.y1)
+        self.data_line_3.setData(self.serial_handler.x,self.serial_handler.y2)
+        self.data_line_4.setData(self.serial_handler.x,self.serial_handler.y3)
+
+            
 
     @QtCore.pyqtSlot()
     def send(self):

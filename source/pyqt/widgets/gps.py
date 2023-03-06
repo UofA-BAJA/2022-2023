fe4e411e6ab097925e4429a3219dc01416d16bb6
LXL1103 <114650659+LXL1103@users.noreply.github.com>
2023-03-05 21:32:22 -0700

    nutz

--- source/pyqt/widgets/gps.py
+++ source/pyqt/widgets/gps.py
@@ -3,43 +3,38 @@ from PyQt5 import QtWidgets, QtGui, QtCore
 from widgets.graph import GraphWidget
 from widgets.tab import GeneralTab
 
-
-
 class GPSWidget(GeneralTab):
     def __init__(self):
         super().__init__()
 
-        
-        
-        self.tab_name = "GPS"
-        
-        self.l = QtWidgets.QLabel()
-        self.l.setText("This is the {self.tab_name} tab")
-        
-        self.layout.addWidget(self.l)
-        
         image_path = r"/Users/man/Downloads/TheUofAmap.png"
+        self.image = QtGui.QPixmap(image_path)
+        
+        
         self.layout = QtWidgets.QVBoxLayout(self)
         self.setLayout(self.layout)
 
-        self.image = QtGui.QPixmap(image_path)
-
-    def paintEvent(self, event):
-
-        painter = QtGui.QPainter(self)
-        #p = QtGui.QPainter()
         
-        #pixmap = QtGui.QPixmap('TheUofAmap.png')
 
-        painter.drawPixmap(self.rect(), pixmap)
-        #y = p.drawPixmap(self.rect(), x)
+        
+        self.tab_name = "GPS"
 
-        #self.layout.addWidget(y)
+        self.l = QtWidgets.QLabel()
+        self.layout.addWidget(self.l)
+
+        
+
+
+       
+    def paintEvent(self, event):
+        
+        painter = QtGui.QPainter(self)
+        painter.drawPixmap(self.rect(), self.image)
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
-    GPSWidget= GPSWidget()
-    GPSWidget.show
+    gpswidget = GPSWidget()
+    gpswidget.show()
     sys.exit(app.exec_())
 
    

30ee7d6de96aee2dd1d78fbdaa811391b5637167
dshay22 <dorianshay0806@gmail.com>
2023-03-12 13:57:16 -0700

    Added graphs

--- source/pyqt/widgets/rpm.py
+++ source/pyqt/widgets/rpm.py
@@ -2,6 +2,8 @@ from PyQt5 import QtWidgets
 
 from widgets.tab import GeneralTab
 
+from widgets.graph import GraphWidget, DataLine
+
 
 class RPMWidget(GeneralTab):
     def __init__(self):
@@ -11,9 +13,33 @@ class RPMWidget(GeneralTab):
         self.setLayout(self.layout)
 
         self.tab_name = "RPMS"
-        
-        self.l = QtWidgets.QLabel()
-        self.l.setText(f"This is the {self.tab_name} tab")
-        self.layout.addWidget(self.l)
 
+        self.setup_graph()
+        
+        # self.l = QtWidgets.QLabel()
+        # self.l.setText(f"This is the {self.tab_name} tab")
+        # self.layout.addWidget(self.l)
+    def setup_graph(self):
+        self.rpmData_right = DataLine("RMP_Front_Right")
+        self.rpmData_right.setPenColor(255,0,0)
+
+        self.rpmData_left = DataLine("RPM_Front_Left")
+        self.rpmData_left.setPenColor(0,255,0)
+        
+        self.rpmData_back = DataLine("RPM_Back")
+        self.rpmData_back.setPenColor(0,0,255)
+
+        self.rpmGrapgh = GraphWidget()
+        self.rpmGrapgh.add_dataline(self.rpmData_right)
+        self.rpmGrapgh.add_dataline(self.rpmData_left)
+        self.rpmGrapgh.add_dataline(self.rpmData_back)
+
+        self.rpmGrapgh.setup()
+        self.layout.addWidget(self.rpmGrapgh)
+
+
+    def updateData(self, data) -> None:
+        self.rpmData_right.update(data.rpm.front_right)
+        self.rpmData_left.update(data.rpm.front_left)
+        self.rpmData_back.update(data.rpm.back)
         
--- source/pyqt/widgets/suspension.py
+++ source/pyqt/widgets/suspension.py
@@ -23,15 +23,34 @@ class SuspensionWidget(GeneralTab):
 
         layout.addWidget(self.config_tab)
         
-
-
         self.layout.addLayout(layout)
 
 
     def setup_graph(self):
+
         self.front_right = DataLine("front_right")
+        self.front_right.setPenColor()
+
+        self.front_left = DataLine("front_left")
+        self.front_left.setPenColor(255, 0, 0)
+
+        self.back_right = DataLine("back_right")
+        self.back_right.setPenColor(255, 0, 255)
+
+        self.back_left = DataLine("back_left")
+        self.back_left.setPenColor(0, 0, 255)
+
         self.suspension_graph = GraphWidget()
         self.suspension_graph.add_dataline(self.front_right)
+        self.suspension_graph.add_dataline(self.front_left)
+        self.suspension_graph.add_dataline(self.back_right)
+        self.suspension_graph.add_dataline(self.back_left)
         self.suspension_graph.setup()
 
         self.layout.addWidget(self.suspension_graph)
+
+    def updateData(self, data):
+        self.front_right.update(data.suspension.front_right)
+        self.front_left.update(data.suspension.front_left)
+        self.back_right.update(data.suspension.back_right)
+        self.back_left.update(data.suspension.back_left)
